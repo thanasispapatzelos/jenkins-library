be41be7301826a95279ef8b024fa4d454696009f
thanasispapatzelos <thanasispapatzelos@gmail.com>
2025-07-08 16:27:15 +0300

    create new function helmWithKubeconfig, handling the right KUBECONFIG

--- vars/helmWithKubeconfig.groovy
+++ vars/helmWithKubeconfig.groovy
@@ -1,6 +1,6 @@
 def call(Closure body) {
-    withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG_FILE')]) {
-        withEnv(["KUBECONFIG=${KUBECONFIG_FILE}"]) {
+    withCredentials([file(credentialsId: 'kubeconfig', variable: 'KUBECONFIG_FILE'),usernamePassword(credentialsId: 'nexus-creds', usernameVariable: 'USERNAME', passwordVariable: 'PASSWORD')]) {
+        withEnv(["KUBECONFIG=$KUBECONFIG_FILE"]) {
             sh '''
                 # Replace IP in kubeconfig
                 sed -i 's|127.0.0.1|host.docker.internal|g' $KUBECONFIG
