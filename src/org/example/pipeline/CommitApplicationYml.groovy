b5c6d5fd43fb58892d31310e0fe73d07bd29eaf1
thanasispapatzelos <thanasispapatzelos@gmail.com>
2025-07-14 11:16:06 +0300

    myPipeline.groovy , now dont deploy the chart. Instead ArgoCD deplying it.

--- src/org/example/pipeline/CommitApplicationYml.groovy
+++ src/org/example/pipeline/CommitApplicationYml.groovy
@@ -13,8 +13,8 @@ class CommitApplicationYml implements Serializable {
                 steps.container('helm') {
                 steps.withCredentials([steps.string(credentialsId: env.GIT_CREDENTIALS_ID , variable: 'GITHUB_PAT')]) {
                     steps.sh """
-                       git config --global --add safe.directory .
-                       git remote set-url origin https://x-access-token:\$GITHUB_PAT@github.com/PapatzelosThanashs/quarkus.git
+                        git config --global --add safe.directory .
+                        git remote set-url origin https://x-access-token:\$GITHUB_PAT@github.com/PapatzelosThanashs/quarkus.git
 
                         git add Application.yml
                         git commit -m "Update Application.yml from pipeline" || echo "No changes"
